774cd06bc2236322febc87afe6a9d0877351129c
junn <wnsdn1207@naver.com>
2021-10-20 17:03:25 +0900

    [BOJ] 14889_스타트와링크 (not solved)

--- src/main/java/own/junn/boj/Problem_14889.java
+++ src/main/java/own/junn/boj/Problem_14889.java
@@ -1,7 +1,21 @@
 package own.junn.boj;
 
-public class Problem_14889 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class Problem_14889 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+        int[][] abilities = new int[N][N];
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            for (int j=0; j<N; j++) {
+                abilities[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        reader.close();
     }
 }
