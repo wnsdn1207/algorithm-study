5dfe2642eecc7327950589f09fdbc5e0710dbd27
junn <wnsdn1207@naver.com>
2021-10-20 15:02:17 +0900

    Init

---
+++ src/main/java/own/junn/boj/Problem_14889.java
@@ -0,0 +1,7 @@
+package own.junn.boj;
+
+public class Problem_14889 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/own/test/boj/Test.java
@@ -0,0 +1,8 @@
+package own.test.boj;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    void test() {
+        System.out.println("Hello World !");
+    }
+}
