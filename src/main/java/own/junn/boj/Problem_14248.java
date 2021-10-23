ca03d0b00366e351ab1fcd37dbf98f5e56eaaf24
Junn <wnsdn1207@naver.com>
2021-10-23 12:23:10 +0900

    [BOJ] 14248_점프점프 (solved)

--- src/main/java/own/junn/boj/Problem_14248.java
+++ src/main/java/own/junn/boj/Problem_14248.java
@@ -8,25 +8,57 @@ import java.io.*;
  */
 public class Problem_14248 {
     static int N;
-    static int[] stones;
     static int[] distances;
     static boolean[] visited;
     static int startPoint;
+
+    static Queue<Integer> queue;
+    static int result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(reader.readLine());
-        stones = new int[N];
         distances = new int[N];
         visited = new boolean[N];
 
         StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
         for (int i=0; i<N; i++) {
-            stones[i] = i+1;
             distances[i] = Integer.parseInt(st.nextToken());
         }
 
-        startPoint = Integer.parseInt(reader.readLine());
+        startPoint = Integer.parseInt(reader.readLine())-1;
+        queue = new LinkedList<>();
+
+        bfs(startPoint);
+        System.out.println(result);
+    }
+
+    static void bfs(int startPoint) {
+        queue.add(startPoint);
+        visited[startPoint] = true;
+        result += 1;
+
+        while (queue.size() > 0) {
+            int node = queue.remove();
+            int distance = distances[node];
+
+            // 왼쪽 점프
+            int jump;
+
+            jump = node - distance;
+            if (jump >= 0 && !visited[jump]) {
+                queue.add(jump);
+                result += 1;
+                visited[jump] = true;
+            }
+
+            jump = node + distance;
+            if (jump < N && !visited[jump]) {
+                queue.add(jump);
+                result += 1;
+                visited[jump] = true;
+            }
+        }
     }
 }
