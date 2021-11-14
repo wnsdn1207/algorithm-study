0af504484b0896139bd2260faaf09374be4f64ed
Junn <wnsdn1207@naver.com>
2021-11-15 00:48:52 +0900

    [BOJ] 10773_제로 (solved, 구현)

---
+++ src/main/java/own/junn/boj/p10773.java
@@ -0,0 +1,40 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * 10773 - 제로
+ */
+public class p10773 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static Stack<Integer> stack = new Stack<>();
+    static int K;
+    public static void main(String[] args) throws Exception{
+        K = Integer.parseInt(reader.readLine());
+
+        for (int i=0; i<K; i++) {
+            int n = Integer.parseInt(reader.readLine());
+
+            if (n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println(0);
+        } else {
+            int result = 0;
+            for (int i : stack) {
+                result += i;
+            }
+            System.out.println(result);
+        }
+
+        reader.close();
+
+    }
+}
