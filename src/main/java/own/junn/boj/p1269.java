f7b119e9e432751d2750dc70f88659157120c249
Junn <wnsdn1207@naver.com>
2021-11-23 00:34:44 +0900

    [BOJ] 1269_대칭차집합 (solved, 자료구조)

---
+++ src/main/java/own/junn/boj/p1269.java
@@ -0,0 +1,45 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+/**
+ * 1269 - 대칭 차집합
+ */
+public class p1269 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static int M;
+
+    static HashMap<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(reader.readLine(), " ");
+        for (int i=0; i<N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        st = new StringTokenizer(reader.readLine(), " ");
+        for (int i=0; i<M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int count = 0;
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        reader.close();
+    }
+}
