9345391bb27ac71981cf8f870770c1446c30462f
junn <wnsdn1207@naver.com>
2021-11-22 12:29:39 +0900

    [BOJ] 5698_Tautogram (solved, 구현/문자열)

---
+++ src/main/java/own/junn/boj/p5698.java
@@ -0,0 +1,37 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 5698 - Tautogram
+ */
+public class p5698 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        String input;
+        while (!(input = reader.readLine()).equalsIgnoreCase("*")) {
+            String[] inputArr = input.split(" ");
+            String first = "";
+
+            boolean isTautogram = true;
+            for (String s : inputArr) {
+                if (first.isEmpty()) {
+                    first = String.valueOf(s.charAt(0));
+                }
+                if (!s.substring(0, 1).equalsIgnoreCase(first)) {
+                    isTautogram = false;
+                }
+            }
+
+            if (isTautogram) {
+                System.out.println("Y");
+            } else {
+                System.out.println("N");
+            }
+        }
+
+        reader.close();
+    }
+}
