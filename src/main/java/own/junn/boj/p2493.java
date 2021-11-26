18420597792c2e17e8a5c16394c8e3373c5c974c
junn <wnsdn1207@naver.com>
2021-11-26 09:57:20 +0900

    [BOJ] 2493_탑 (solved, 스택, 다음에는 Stack을 int 배열로 구현해서 풀어보자)

---
+++ src/main/java/own/junn/boj/p2493.java
@@ -0,0 +1,56 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * 2493 - 탑
+ */
+public class p2493 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static int[] tops;
+    static int[] indexes;
+    static Stack<Integer> stack = new Stack<>();
+    static Stack<Integer> indexStack = new Stack<>();
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(reader.readLine());
+        tops = new int[N];
+        indexes = new int[N];
+
+        StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+        for (int i=0; i<N; i++) {
+            tops[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx = N;
+
+        while (idx-- > 0) {
+            while (!stack.isEmpty()) {
+                int n = stack.peek();
+
+                if (n < tops[idx]) {
+                    stack.pop();
+                    indexes[indexStack.pop()] = idx+1;
+                } else {
+                    break;
+                }
+            }
+            stack.add(tops[idx]);
+            indexStack.add(idx);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int n : indexes) {
+            sb.append(n).append(" ");
+        }
+
+        System.out.println(sb);
+
+        reader.close();
+    }
+}
