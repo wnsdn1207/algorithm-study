5ae6d0817c3dc9ea8bb8f407fa1521008d1806a4
junn <wnsdn1207@naver.com>
2021-12-06 12:52:42 +0900

    [BOJ] 10870_피보나치수_5 (solved, DP)

---
+++ src/main/java/own/junn/boj/p10870.java
@@ -0,0 +1,30 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 10870 - 피보나치수 5
+ */
+public class p10870 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static int[] dp;
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(reader.readLine());
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i=2; i<dp.length; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
