4736a8a1969dee9e47e279eb8963c0fb01cc00bd
junn <wnsdn1207@naver.com>
2021-12-06 13:13:30 +0900

    [BOJ] 2839_설탕배달 (solved, 그리디)

---
+++ src/main/java/own/junn/boj/p2839.java
@@ -0,0 +1,37 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 2839 - 설탕 배달
+ */
+public class p2839 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(reader.readLine());
+
+        int result = 0;
+        while (true) {
+            if (N % 5 > 0) {
+                N -= 3;
+            } else {
+                N -= 5;
+            }
+            result++;
+
+            if (N == 0) {
+                break;
+            } else if (N < 3) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        System.out.println(result);
+
+        reader.close();
+    }
+}
