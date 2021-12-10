cbdc89d998d21d5bdb4ef00efe8f70ab691ec5ef
Junn <wnsdn1207@naver.com>
2021-12-11 03:18:05 +0900

    [BOJ] 5585_거스름돈 (solved, 그리디)

---
+++ src/main/java/own/junn/boj/p5585.java
@@ -0,0 +1,33 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 5585 - 거스름돈
+ */
+public class p5585 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+
+    static int[] penny = {500, 100, 50, 10, 5, 1};
+    public static void main(String[] args) throws Exception {
+        N = 1000 - Integer.parseInt(reader.readLine());
+
+        int answer = 0;
+        int idx = 0;
+        while (N > 0) {
+            if (N >= penny[idx]) {
+                N -= penny[idx];
+                answer++;
+            } else {
+                idx++;
+            }
+        }
+
+        System.out.println(answer);
+
+        reader.close();
+    }
+}
