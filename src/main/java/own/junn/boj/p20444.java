32e0d5dede4da51d995f6910c3484796f7170052
junn <wnsdn1207@naver.com>
2021-12-17 14:47:52 +0900

    [BOJ] 20444_색종이와가위 (solved, 이진탐색, 개선 필요, 설명있음)
    - 정수형 계산 시, double 타입을 사용하면 성능이 당연히 저하됨

---
+++ src/main/java/own/junn/boj/p20444.java
@@ -0,0 +1,48 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 20444 - 색종이와 가위
+ *
+ * 예제 입력 1
+ * 4 9
+ * 예제 출력 1
+ * YES
+ *
+ * 예제 입력 2
+ * 4 6
+ * 예제 출력 2
+ * NO
+ */
+public class p20444 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static long N;
+    static long K;
+
+    public static void main(String[] args) throws Exception {
+        String[] input = reader.readLine().split(" ");
+
+        N = Long.parseLong(input[0]);
+        K = Long.parseLong(input[1]);
+
+        long height=1, width=1;
+
+        for (long i=N; i>=N/2; i--) {
+            long ah = height + i;
+            long aw = width + (N - i);
+
+            long ahw = ah * aw;
+            if (ahw == K) {
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+
+        reader.close();
+    }
+}
