b57f7d062053d7e6555c2ee575c08099aa5a1eff
junn <wnsdn1207@naver.com>
2021-12-30 13:24:16 +0900

    [BOJ] 10998_AxB (solved)

---
+++ src/main/java/own/junn/boj/p10998.java
@@ -0,0 +1,20 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 10998 - AxB
+ */
+public class p10998 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+
+        System.out.println(Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+
+        reader.close();
+    }
+}
