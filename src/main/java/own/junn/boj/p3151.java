495cda78efdb61073db3cf9a60ee18a1a1ac78a5
junn <wnsdn1207@naver.com>
2022-01-03 17:49:58 +0900

    [BOJ] 3151_합이0 (not solved)

---
+++ src/main/java/own/junn/boj/p3151.java
@@ -0,0 +1,17 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 3151 - 합이 0
+ */
+public class p3151 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        reader.readLine();
+
+        reader.close();
+    }
+}
