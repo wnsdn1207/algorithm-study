a3f0c9264bebfbf5bf80017530ed8dcbdd6364eb
Junn <wnsdn1207@naver.com>
2022-02-16 19:38:28 +0900

    [BOJ] 1152_단어의개수 (solved, 문자열)

---
+++ src/main/java/own/junn/boj/p1152.java
@@ -0,0 +1,36 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 1152 - 단어의 개수
+ */
+public class p1152 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        String input = reader.readLine();
+
+        int count = 0;
+
+        boolean isWord = false;
+        for (int i=0; i<input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c == ' ') {
+                isWord = false;
+            }
+
+            if (isWord) continue;
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                count++;
+                isWord = true;
+            }
+        }
+
+        System.out.println(count);
+
+        reader.close();
+    }
+}
