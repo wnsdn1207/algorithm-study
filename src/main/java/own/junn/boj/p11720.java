60bcef5841fae74fd3f2a788b400dcf63e5c81e3
Junn <wnsdn1207@naver.com>
2022-02-16 18:06:57 +0900

    [BOJ] 11720_숫자의합 (solved, 문자열)

---
+++ src/main/java/own/junn/boj/p11720.java
@@ -0,0 +1,28 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 11720 - 숫자의 합
+ */
+public class p11720 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static char[] chars;
+    
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(reader.readLine());
+        chars = reader.readLine().toCharArray();
+
+        int result = 0;
+        for (char c : chars) {
+            result += (c - '0');
+        }
+
+        System.out.println(result);
+
+        reader.close();
+    }
+}
