17f4468206a156d0948c14ce5587405570258da8
Junn <wnsdn1207@naver.com>
2022-02-17 00:06:12 +0900

    [BOJ] 2908_상수 (solved, 문자열)

---
+++ src/main/java/own/junn/boj/p2908.java
@@ -0,0 +1,31 @@
+package own.junn.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 2908 - 상수
+ */
+public class p2908 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        String[] input = reader.readLine().split(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        int A, B;
+
+        sb.append(input[0]);
+        A = Integer.parseInt(sb.reverse().toString());
+        sb.setLength(0);
+
+        sb.append(input[1]);
+        B = Integer.parseInt(sb.reverse().toString());
+        sb.setLength(1);
+
+        System.out.println(Math.max(A, B));
+
+        reader.close();
+    }
+}
