21b7df07d891d6955d4a38a8060c0e4f5410b2ce
junify <wnsdn1207@naver.com>
2023-07-16 17:59:21 +0900

    [BOJ] 1120 - 문자열 - 1차 시도

--- src/main/java/own/junn/boj/p1120.java
+++ src/main/java/own/junn/boj/p1120.java
@@ -4,38 +4,33 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 1120 - 문자열
- */
 public class p1120 {
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-    static String A;
-    static String B;
     public static void main(String[] args) throws Exception {
         StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
 
-        A = st.nextToken();
-        B = st.nextToken();
+        char[] aTextArr = st.nextToken().toCharArray();
+        char[] bTextArr = st.nextToken().toCharArray();
 
-        if (B.contains(A)) {
-            System.out.println("0");
-        } else {
-            if (B.charAt(0) == A.charAt(0)) {
-                A = B.substring((B.length() - A.length()) - 1);
-                System.out.println(A);
-            } else if (B.charAt(B.length()-1) == A.charAt(A.length()-1)) {
-                A = B.substring(0, B.length() - A.length()) + A;
-                System.out.println(A);
-            } else if (A.length() == B.length()) {
-                System.out.println(A);
-            } else {
+        int eventCount = bTextArr.length - aTextArr.length;
 
+        int result = Integer.MAX_VALUE;
+        for (int i=0; i<=eventCount; i++) {
+            result = Math.min(result, getDiffCount(aTextArr, bTextArr, i));
+        }
+
+        System.out.println(result);
+    }
+
+    private static int getDiffCount(char[] arr1, char[] arr2, int idx) {
+        int diffCount = 0;
+
+        for (int i=0; i<arr1.length; i++) {
+            if (arr1[i] != arr2[i+idx]) {
+                diffCount++;
             }
         }
 
-
-
-        reader.close();
+        return diffCount;
     }
 }
