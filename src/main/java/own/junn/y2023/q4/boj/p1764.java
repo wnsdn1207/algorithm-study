96e4b4715ce45a7c0e2c7153b99b71b8cf9c0159
Junn <wnsdn1207@naver.com>
2023-12-29 23:31:56 +0900

    [백준] p1764 solving

---
+++ src/main/java/own/junn/y2023/q4/boj/p1764.java
@@ -0,0 +1,15 @@
+package own.junn.y2023.q4.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class p1764 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        reader.readLine();
+
+        reader.close();
+
+    }
+}
