9826e36880ccbdb831f5abe5a8d80af9d712fff2
Junn <wnsdn1207@naver.com>
2023-12-30 13:43:12 +0900

    [백준] p1764 solved

--- src/main/java/own/junn/y2023/q4/boj/p1764.java
+++ src/main/java/own/junn/y2023/q4/boj/p1764.java
@@ -2,14 +2,39 @@ package own.junn.y2023.q4.boj;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class p1764 {
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws Exception {
-        reader.readLine();
+        StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // Set을 통해 중복 여부를 체크하고, PriorityQueue를 통해 사전순 출력을 한다.
+        Set<String> set = new HashSet<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+
+        // 듣도 못한 사람
+        for (int i=0; i<N; i++) {
+            set.add(reader.readLine());
+        }
+
+        // 보도 못한 사람
+        for (int i=0; i<M; i++) {
+            String name = reader.readLine();
+            if (set.contains(name)) {
+                priorityQueue.add(name);
+            }
+        }
+
+        System.out.println(priorityQueue.size());
+        while (priorityQueue.size() > 0) {
+            System.out.println(priorityQueue.poll());
+        }
 
         reader.close();
-
     }
 }
