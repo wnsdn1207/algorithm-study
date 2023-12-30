00fa03747d75a8bd84c0fd7003753ef70fe92b59
Junn <wnsdn1207@naver.com>
2023-12-30 22:19:14 +0900

    [백준] p20920 solved - 2

---
+++ src/main/java/own/junn/y2023/q4/boj/p20920_2.java
@@ -0,0 +1,84 @@
+package own.junn.y2023.q4.boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class p20920_2 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(reader.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, Word> map = new HashMap<>();
+        for (int i=0; i<N; i++) {
+            String word = reader.readLine();
+            if (word.length() < M) {
+                continue;
+            }
+            map.computeIfAbsent(word, k -> new Word(word, word.length()))
+                    .incrementCount();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        TreeSet<Word> treeSet = new TreeSet<>(map.values());
+        for (Word word : treeSet) {
+            sb.append(word.word).append("\n");
+        }
+
+        writer.write(sb.toString());
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+
+    public static class Word implements Comparable<Word> {
+
+        private final String word;
+
+        private final int length;
+
+        private int count;
+
+        public Word(String word, int length) {
+            this.word = word;
+            this.length = length;
+            this.count = 0;
+        }
+
+        public void incrementCount() {
+            this.count += 1;
+        }
+
+        @Override
+        public int compareTo(Word o) {
+            int countDiff = o.count - this.count;
+            if (countDiff != 0) {
+                return countDiff;
+            }
+
+            int lengthDiff = o.length - this.length;
+            if (lengthDiff != 0) {
+                return lengthDiff;
+            }
+
+            char[] originChars = this.word.toCharArray();
+            char[] comparisonChars = o.word.toCharArray();
+
+            for (int i=0; i<originChars.length; i++) {
+                int charDiff = originChars[i] - comparisonChars[i];
+                if (charDiff != 0) {
+                    return charDiff;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
